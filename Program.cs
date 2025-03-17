6799d65fb5ccc3830c35c1ccac23e0abce5f05e2
chuzhic <kabanovaleksej326@gmail.com>
2025-03-17 20:41:39 +0500

    problemka

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("надобно ввести 1 число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("и второе тоже надобно");
+int  b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"a & b = {a & b}");
+Console.WriteLine($"a | b = {a | b}");
+Console.WriteLine($"a ^ b = {a ^ b}");
+
+Console.WriteLine($"b << 2 = {b << 2}");
+
+Console.WriteLine($"a >> 2 = {a >> 2}");
